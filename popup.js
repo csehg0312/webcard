5f5024158c80173d8c5420e22c95af3162449c6f
csehg0312 <csehgabor451@gmail.com>
2023-07-07 19:20:55 +0200

    Made changes

--- popup.js
+++ popup.js
@@ -6,8 +6,14 @@ function openPopup(link) {
   // popupWindow.moveTo(100, 100);
   const qrCode = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${link}`;
   const qrImage = document.getElementById('qr-place');
-  console.log(qrImage.src);
-  console.log(qrCode);
   qrImage.src = String(qrCode);
-  console.log(qrImage.src);
+ 
+}
+
+function openSteps() {
+  const stepo = '';
+  const stept = '';
+  const stepth = '';
+
+  
 }
