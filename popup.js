59a7ce756e604021b175374355b79de1a14bc6b1
csehg0312 <csehgabor451@gmail.com>
2023-07-14 15:45:19 +0200

    Started expense tracker

--- popup.js
+++ popup.js
@@ -9,11 +9,62 @@ function openPopup(link) {
   qrImage.src = String(qrCode);
  
 }
+alertPlaceholder = document.getElementById("AlertPlaceHolder");
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
 
-function openSteps() {
-  const stepo = '';
-  const stept = '';
-  const stepth = '';
+  alertPlaceholder.append(wrapper)
+}
 
+const Account = {
+  balance: 0,
+  // add: this.balance = (money) => this.balance + money,
+  set add(money) {
+    this.balance = this.balance + money;
+  }
+  // reduce: this.balance = (money) => this.balance - money,
+
+}
+
+const SavingAcc = {
+  balance: 0,
+  type: "",
+  // add: this.balance = (money) => this.balance + money,
+  set add(money) {
+    this.balance = this.balance + money;
+  },
   
+  set clearAcc(att) {
+    this.balance = att;
+  }
+  // reduce: this.balance = (money) => this.balance - money,
+}
+
+document.getElementById("balanceAcc").innerHTML = Account.balance;
+document.getElementById("balanceSavAcc").innerHTML = SavingAcc.balance;
+
+// AccButton = document.getElementById("Acc");
+// SavAccbButton = document.getElementById("savAcc");
+
+// AccButton.addEventListener('click', hello);
+// SavAccbButton.addEventListener('click', hello);
+
+function clearIt(){
+  Account.clearAcc = 0;
+  document.getElementById("balanceAcc").innerHTML = Account.balance;
+}
+
+function account_add(){
+  // let outputText = document.getElementById("balanceAcc");
+  let inputAccount = document.getElementById("inputAccount");
+
+  let input = parseInt(inputAccount.value);
+  if (isNaN(input)){
+    alert('Just used bad type of data.\nRather use numbers.', 'danger');
+  }else{
+    Account.add = input;
+    // console.log(Account.balance);
+    document.getElementById("balanceAcc").innerHTML = Account.balance;
+  }
 }
